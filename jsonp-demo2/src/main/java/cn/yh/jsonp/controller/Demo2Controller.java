8aa82726e68c847ff04c298fe26e5c17b35a1de0
springyh <springyh@yeah.net>
2018-06-14 22:34:09 +0800

    更改了controller中说明

--- jsonp-demo2/src/main/java/cn/yh/jsonp/controller/Demo2Controller.java
+++ jsonp-demo2/src/main/java/cn/yh/jsonp/controller/Demo2Controller.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Demo2Controller {
+	/*
+		这是访问index页面的控制器
+		我的博客地址是:www.csdn.cn
+	*/
 	@RequestMapping("/")
 	public String index() {
 		return "index";
